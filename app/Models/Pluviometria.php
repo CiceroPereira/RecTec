71d5a0f5e26b3244cff1f877f158a95315904fd2
cicero.junior <cicerojrlima@hotmail.com>
2019-02-14 17:14:18 -0200

    Correção de caminho

--- app/Models/Pluviometria.php
+++ app/Models/Pluviometria.php
@@ -14,6 +14,6 @@ class Pluviometria extends Model
     }
 
     public function pluviometro(){
-    	return $this->belongsTo('App\Models\pluviometro', 'pluviometro_id');
+    	return $this->belongsTo('App\Models\Pluviometro', 'pluviometro_id');
     }
 }
