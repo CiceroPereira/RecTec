82057317c1fdf90ba72c502705b38af14812ae92
cicero.junior <cicerojrlima@hotmail.com>
2019-08-14 11:18:55 -0300

    .

--- app/Http/Controllers/PluviometriaController.php
+++ app/Http/Controllers/PluviometriaController.php
@@ -135,7 +135,7 @@ class PluviometriaController extends Controller
                 $timestamp = strtotime($dateHour); 
                 $timestamp = $timestamp*1000;
 
-                $url = 'http://172.16.68.21:8080/api/v1'.$accessToken.'/telemetry';
+                $url = 'http://172.16.68.21:8080/api/v1/'.$accessToken.'/telemetry';
                 $client = new Client(); //GuzzleHttp\Client
                 $result = $client->request('POST',$url, ['json' => ['ts' => $timestamp, 
                     'values' => ['lamina' => $pluviometrias->lamina]]
