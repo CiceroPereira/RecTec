6d05b287aa7856030ef6455fbd0d30eb84bebcff
cicero.junior <cicerojrlima@hotmail.com>
2019-08-14 10:47:38 -0300

    ponto e virgula...

--- app/Http/Controllers/PluviometriaController.php
+++ app/Http/Controllers/PluviometriaController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
 use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Client
+use GuzzleHttp\Client;
 
 class PluviometriaController extends Controller
 {
